ef93e0324d02500dfcc07a0f0d288348a8d7beaf
langerZeit <1014923624@qq.com>
2019-11-11 18:10:20 +0800

    第一次试试

--- test.py
+++ test.py
@@ -1,1 +1,5 @@
 print('Hello World!')
+print("""When other men blindly follow the truth,remember...Nothing is true
+        When other men are limited,by morality or law,remember...Everything is permitted.
+        We work in the dark to serve the light.We are assassins.
+        Nothing is true,everything is permitted.""") #by 杨某人
